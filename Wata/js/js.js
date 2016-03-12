e9b485889f7f6626a7a58f3032bf35a8bf47ee17
Cüneyt Yılmaz <cuneyilmaz@gmail.com>
2016-03-12 18:52:56 +0200

    Fiyatlar kısmı eklendi. Radio button eklendi. Radiobutton'un javascriptleri eklendi.

--- Wata/js/js.js
+++ Wata/js/js.js
@@ -17,3 +17,17 @@ $('#myTabs a[href="#profile"]').tab('show') // Select tab by name
 $('#myTabs a:first').tab('show') // Select first tab
 $('#myTabs a:last').tab('show') // Select last tab
 $('#myTabs li:eq(2) a').tab('show') // Select third tab (0-indexed)
+
+
+$('input[name="rb-fiyat"]').on('change', function () {
+    if ($(this).is(':checked') && $(this).val() == '$') {
+        //change to "show update"
+        $("#fiyat1").text("$19");
+        $("#fiyat2").text("$49");
+        $("#fiyat3").text("$99");
+    } else  if($(this).is(':checked') && $(this).val() == '€'){
+        $("#fiyat1").text("€17");
+        $("#fiyat2").text("€43");
+        $("#fiyat3").text("€87");
+    }
+});
